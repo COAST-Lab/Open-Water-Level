43b0513e1bfc1bba190b3bb9315f855b99aa00a5
pjb-mbp15 <pjbresnahan@gmail.com>
2021-06-24 17:34:23 -0400

    Add SD card logging

--- Firmware/SLR_Boron_Maxbotix_Analog/src/SLR002_Maxbotix_Analog.cpp
+++ Firmware/SLR_Boron_Maxbotix_Analog/src/SLR002_Maxbotix_Analog.cpp
@@ -4,13 +4,26 @@
 
 #include "Particle.h"
 #line 1 "/Users/pjb/Dropbox/Particle_Projects/Seaport_SLR_Observation/Seaport_Tide-SLR_Maxbotix_Arduino/Firmware/SLR_Boron_Maxbotix_Analog/src/SLR002_Maxbotix_Analog.ino"
+#include "SdFat.h"
+//------------------SD SPI Configuration Details--------------------------------
 void setup(void);
 void loop(void);
-#line 1 "/Users/pjb/Dropbox/Particle_Projects/Seaport_SLR_Observation/Seaport_Tide-SLR_Maxbotix_Arduino/Firmware/SLR_Boron_Maxbotix_Analog/src/SLR002_Maxbotix_Analog.ino"
-int V_ultraS = A0;
+#line 3 "/Users/pjb/Dropbox/Particle_Projects/Seaport_SLR_Observation/Seaport_Tide-SLR_Maxbotix_Arduino/Firmware/SLR_Boron_Maxbotix_Analog/src/SLR002_Maxbotix_Analog.ino"
+const int SD_CHIP_SELECT = SS;
+SdFat sd;
 
-// SYSTEM_THREAD(ENABLED);
+//------------------LED Setup
+int led = D7; // blink to let us know you're alive
+bool led_state = HIGH; // starting state
+
+//------------------Maxbotix sensor
+int V_ultraS = A0;
+float dist_in = 0;
+
+
+//------------------Turn off cellular for prelim testing
 SYSTEM_MODE(MANUAL);
+// SYSTEM_THREAD(ENABLED);
 
 void setup(void)
 {
@@ -25,18 +38,39 @@ void setup(void)
 
 void loop(void)
 {
-  
-  Serial.print("Distance(in): ");
+  // Read analog value and convert
   //Counts * (V range/count range) * (Inch range/V range)
-  Serial.print(analogRead(V_ultraS)*(3.3/2048)*(254/3.3)); 
+  dist_in = analogRead(V_ultraS)*(3.3/2048)*(254/3.3);
 
-//   float distanceInches = distance * 0.0393701;
-//   float distanceFeet = distanceInches / 12.0;
+  // Print out distance
+  Serial.print("Time: ");
+  Serial.print(Time.now());
+  Serial.print(", Distance(in): ");
+  
+  Serial.println(dist_in);
 
-//   Serial.print(", Distance(ft): ");
-//   Serial.print(distanceFeet, 2);
+  // Start SD stuff
+	File myFile;
 
-  Serial.println();
+	// Initialize the library
+	if (!sd.begin(SD_CHIP_SELECT, SPI_FULL_SPEED)) {
+		Serial.println("failed to open card");
+		return;
+	}
 
-  delay(1000);
+	// open the file for write at end like the "Native SD library"
+	if (!myFile.open("distance.txt", O_RDWR | O_CREAT | O_AT_END)) {
+		Serial.println("opening test.txt for write failed");
+		return;
+	}
+
+  // Save to SD card
+  myFile.print(Time.now());
+  myFile.print(",");
+  myFile.print(millis());
+  myFile.print(",");
+  myFile.println(dist_in);
+  myFile.close();
+
+  delay(5000);
 }
--- Firmware/SLR_Boron_Maxbotix_Analog/src/SLR002_Maxbotix_Analog.ino
+++ Firmware/SLR_Boron_Maxbotix_Analog/src/SLR002_Maxbotix_Analog.ino
@@ -1,7 +1,20 @@
-int V_ultraS = A0;
+#include "SdFat.h"
+//------------------SD SPI Configuration Details--------------------------------
+const int SD_CHIP_SELECT = SS;
+SdFat sd;
 
-// SYSTEM_THREAD(ENABLED);
+//------------------LED Setup
+int led = D7; // blink to let us know you're alive
+bool led_state = HIGH; // starting state
+
+//------------------Maxbotix sensor
+int V_ultraS = A0;
+float dist_in = 0;
+
+
+//------------------Turn off cellular for prelim testing
 SYSTEM_MODE(MANUAL);
+// SYSTEM_THREAD(ENABLED);
 
 void setup(void)
 {
@@ -16,18 +29,39 @@ void setup(void)
 
 void loop(void)
 {
-  
-  Serial.print("Distance(in): ");
+  // Read analog value and convert
   //Counts * (V range/count range) * (Inch range/V range)
-  Serial.print(analogRead(V_ultraS)*(3.3/2048)*(254/3.3)); 
+  dist_in = analogRead(V_ultraS)*(3.3/2048)*(254/3.3);
 
-//   float distanceInches = distance * 0.0393701;
-//   float distanceFeet = distanceInches / 12.0;
+  // Print out distance
+  Serial.print("Time: ");
+  Serial.print(Time.now());
+  Serial.print(", Distance(in): ");
+  
+  Serial.println(dist_in);
 
-//   Serial.print(", Distance(ft): ");
-//   Serial.print(distanceFeet, 2);
+  // Start SD stuff
+	File myFile;
 
-  Serial.println();
+	// Initialize the library
+	if (!sd.begin(SD_CHIP_SELECT, SPI_FULL_SPEED)) {
+		Serial.println("failed to open card");
+		return;
+	}
 
-  delay(1000);
+	// open the file for write at end like the "Native SD library"
+	if (!myFile.open("distance.txt", O_RDWR | O_CREAT | O_AT_END)) {
+		Serial.println("opening test.txt for write failed");
+		return;
+	}
+
+  // Save to SD card
+  myFile.print(Time.now());
+  myFile.print(",");
+  myFile.print(millis());
+  myFile.print(",");
+  myFile.println(dist_in);
+  myFile.close();
+
+  delay(5000);
 }
